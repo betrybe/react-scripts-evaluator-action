4ecd2e2e08a69ee378bf77e39609319161601a86
Jean Paulo Silva Vasconcelos <jean.vasconcelos6@gmail.com>
2020-10-27 14:21:33 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ rm -rf /project-tests/.git
 cp -r /project-tests/* .
 npm install
 npm install -g react-scripts
-CI=true react-scripts test --json --outputFile=evaluation.json
+react-scripts test --watchAll=false --json --outputFile=evaluation.json
 node /evaluator.js evaluation.json .trybe/requirements.json result.json
 
 if [ $? != 0 ]; then
